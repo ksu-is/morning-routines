5b9c1d008c2d0167fd2a6822d72dc97ac2428239
VikeP64 <princessvikeilia1@gmail.com>
2024-04-21 13:17:17 -0400

    Update FinalProjectVikeiliaPembertionHenry.py
    Started the project with options for morning routines

---
+++ FinalProjectVikeiliaPembertionHenry.py
@@ -0,0 +1,11 @@
+name = input("Hello! Please tell me your name")
+print("Hello ", name, " Would you like a morning routine or night routine? Please press 1 for morning or 2 for night")
+morn= input("Enter the number corresponding to your choice: ")
+def display_morning_menu():
+    print("Morning Routine Menu:")
+    print("1. Exercise")
+    print("2. Meditate")
+    print("3. Read")
+    print("4. Breakfast")
+    print("5. Take a Shower")
+    print("6. Quit")
